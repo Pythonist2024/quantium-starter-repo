f6678fc90dc8c051ad1d609fbd4112f027be54a8
Pythonist2024 <meltemtextilero@gmail.com>
2024-02-14 09:13:11 +0200

    Third commit

---
+++ app3.py
@@ -0,0 +1,23 @@
+from dash import Dash, dcc, html 
+import pandas as pd 
+import plotly.express as px 
+
+df = pd.read_csv("C:/Users/user3/Desktop/formatted_data.csv")
+
+df = df.sort_values(by = "date")
+
+app = Dash(__name__)
+
+fig = px.line(df, x = "date", y = "sales", title = "Pink Morsel Sales", color = "region")
+
+app.layout = html.Div([
+    html.H1(style = {'textAlign' : 'center'}, children = "Pink Morsel Visualization"),
+     
+    dcc.Graph(
+       id ='Pink Morsel',
+       figure = fig
+    )
+     ])
+   
+if __name__ == "__main__":
+   app.run(debug = True)   
